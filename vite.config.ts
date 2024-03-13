c7f72b80e6646f40b5cc18a878f0cb108f9e594c
кирилл <shakhov-work@yandex.ru>
2024-03-13 11:15:12 +0300

    master feat( vite.config.ts ):added alias

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      // Устанавливаем псевдоним '@' для пути к каталогу 'src'
+      '@': fileURLToPath(new URL('src', import.meta.url))
+    }
+  }
+})
