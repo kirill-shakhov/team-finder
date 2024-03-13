8fdce408d49325ce5b6797c12adbb3b971d1c985
кирилл <shakhov-work@yandex.ru>
2024-03-13 11:48:16 +0300

    master feat(vue-router ): initialization

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
   <div>
-    1
+    <RouterView />
   </div>
 </template>
 
@@ -14,9 +15,11 @@
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
