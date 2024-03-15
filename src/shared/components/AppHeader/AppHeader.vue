d05bb615b1ea33e57da97b590c94711809a61718
кирилл <shakhov-work@yandex.ru>
2024-03-15 11:21:06 +0300

    master feat( AppHeader.vue ): added component

---
+++ src/shared/components/AppHeader/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="app-header">
+    <h1 class="app-header__title">
+      Жилфонд
+    </h1>
+
+    <h2 class="app-header__text">
+      Пользователь
+    </h2>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  .app-header{
+    padding: 50px 0 25px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+
+    @include tablet{
+      flex-direction: row;
+    }
+  }
+
+  .app-header__title {
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 39px;
+    color: $primary;
+  }
+
+  .app-header__text {
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    color: $darkGrey;
+  }
+</style>
