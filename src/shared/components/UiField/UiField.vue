a7275396d81aebc35891e8f4d192ae9c4cc54e10
кирилл <shakhov-work@yandex.ru>
2024-03-15 11:22:38 +0300

    master feat( UiField ): added component

---
+++ src/shared/components/UiField/UiField.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="ui-field">
+    <label v-if="label" class="ui-field__label">{{ label }}</label>
+
+    <div class="ui-field__content">
+      <slot/>
+    </div>
+  </div>
+
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  label?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  label: '',
+});
+</script>
+
+<style src="./UiField.styles.scss" lang="scss"></style>
