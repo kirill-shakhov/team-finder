2db181027c97b281dde153c13da8dd4ff31f62cc
кирилл <shakhov-work@yandex.ru>
2024-03-15 11:39:20 +0300

    master feat( main ): added store

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import '../style.scss';
 import App from './App.vue';
 import router from '@/router';
+import { store, rootStoreKey }  from '@/store';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store, rootStoreKey);
 
 app.mount('#app');
