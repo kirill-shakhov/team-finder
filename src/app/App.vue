1c45723a77df3232a641799af7291611999a9f3f
кирилл <shakhov-work@yandex.ru>
2024-03-15 11:50:13 +0300

    master feat( App ): added container

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,24 +3,10 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <RouterView />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
