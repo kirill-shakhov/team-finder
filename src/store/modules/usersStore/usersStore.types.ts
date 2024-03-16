ae4c4a217b1c5a35be3b2eb87ee91c5bed60025c
кирилл <shakhov-work@yandex.ru>
2024-03-16 14:48:30 +0300

    master feat( usersStore ): added state type

--- src/store/modules/usersStore/usersStore.types.ts
+++ src/store/modules/usersStore/usersStore.types.ts
@@ -1,5 +1,6 @@
-import { GetUsersResponse } from '@/services/api/controllers';
+import { GetUsersResponse, User } from '@/services/api/controllers';
 
 export interface UsersState {
   users: GetUsersResponse | [];
+  currentUser: User | null;
 }
